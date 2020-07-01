e58ec1975f705fcb40a37e0635701ac4c87f5e8c
dave <dave@thecoderscorner.com>
2020-07-01 11:48:12 +0100

    New project to check over tcmenu java ui deployments, experimental simhub support, more project configuration added, including for ESP boards.

---
+++ mbedMenuJava/tcmenu_main.cpp
@@ -0,0 +1,36 @@
+#include <mbed.h>
+#include "mbedMenuJava_menu.h"
+
+I2C i2c(PF_0, PF_1);
+
+Serial console(USBTX, USBRX);
+MBedLogger LoggingPort(console);
+
+void setup() {
+    setupMenu();
+
+}
+
+int main() {
+    setup();
+    while(1) {
+        taskManager.runLoop();
+    }
+}
+
+
+
+
+
+
+void CALLBACK_FUNCTION onUserButton(int id) {
+    // TODO - your menu change code
+}
+
+
+
+
+
+
+
+
