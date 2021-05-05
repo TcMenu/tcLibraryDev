ae777e75833d56ba25e1f1d5693e1656aaa51c80
Dave Cherry <dave@nutricherry.com>
2021-05-05 12:53:12 +0100

    Test for developing AVR direct

--- avrTestTwi/avrTestWire.cpp
+++ avrTestTwi/avrTestWire.cpp
@@ -2,6 +2,23 @@
 #include <IoAbstraction.h>
 #include <PlatformDeterminationWire.h>
 
+uint8_t toWrite = 0;
+
+void checkAllI2CAddresses() {
+    Serial.println("Starting scan");
+    for(int i=0; i<128; i++) {
+        if(ioaWireReady(defaultWireTypePtr, i)) {
+            Serial.print("Device found on 0x");
+            Serial.println(i, HEX);
+        }
+        else {
+            Serial.print("Nothing at 0x");
+            Serial.println(i, HEX);
+        }
+    }
+    Serial.println("Finished scan");
+}
+
 void setup() {
     Serial.begin(115200);
     pinMode(LED_BUILTIN, OUTPUT);
@@ -12,9 +29,11 @@ void setup() {
 
     Serial.println("Wire begin completed");
 
-    bool ready = ioaWireReady(defaultWireTypePtr, 0x20);
-
-    Serial.print("wire ready "); Serial.println(ready);
+    taskManager.scheduleOnce(100, checkAllI2CAddresses);
+    taskManager.scheduleFixedRate(1000, [] {
+        toWrite = toWrite!=0 ? 0 : 0xff;
+        ioaWireWriteWithRetry(defaultWireTypePtr, 0x20, &toWrite, 1);
+    });
 }
 
 void loop() {
