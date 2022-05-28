25abba85c274db62fd0ea15a4beb118c1a760570
Dave Cherry <dave@nutricherry.com>
2022-05-28 10:00:09 +0100

    updated projects

--- experimental/experimental.cpp
+++ experimental/experimental.cpp
@@ -1,53 +1,47 @@
-/**
- * My environment is platformIO (CLion IDE), I've tried this on various versions of the Stm32Duino platform back to
- * about a year ago. In one project it reliably fails, in the other it works perfectly.
- *
- * Unfortunately, I cannot run the sketch in Arduino as I'd have to add the board I'm using to the boards file there.
- * I know the board is working properly when the marked line below is commented out.
- *
- * It's possible I've made a mistake and would be very very glad to stand corrected, or even that the problem lies
- * within my build chain (although I've got this problem on two devices), or even that there is a problem elsewhere
- * in the tool chain.
- */
+#include <TaskManagerIO.h>
+#include "IoLogging.h"
 
-#include <Arduino.h>
-
-// First we create an interface that we will use later - to create a virtual table.
-class MyAbstraction {
+class EventTesting : public BaseEvent {
+private:
+    uint32_t counter = 0;
+    const char* name;
 public:
-    virtual void pinDirection(uint8_t pin, uint8_t mode)=0;
-};
+    explicit EventTesting(const char *name) : name(name) {}
 
-// Then we create the implementation that has the pinDirection method implementation.
-class MyAbstractionImpl : public MyAbstraction {
-public:
-    void pinDirection(uint8_t pin, uint8_t mode) override {
-        pinMode(pin, mode);
+    void exec() override {
+        counter++;
+        serdebugF3("exec - counter is ", counter, name);
+    }
+
+    uint32_t timeOfNextCheck() override {
+
+        return 60UL * 1000000UL;
     }
 };
 
-volatile int counter;
+EventTesting testing1("test1");
+EventTesting testing2("test2");
 
-void interruptCallback() {
-    // in the interrupt we just increment the counter
-    counter++;
-}
+uint32_t thenTime1;
+uint32_t thenTime2;
 
 void setup() {
-    // commenting out this line makes the sketch work in the environment that fails.
-    auto* digitalIo = new MyAbstractionImpl();
-    Serial.begin(115200);
-
-    // If I replace the above with the below line everything works as expected and the interrupt is registered.
-    pinMode(PC8, INPUT_PULLUP);
-
-    // here we register the interrupt
-    ::attachInterrupt(digitalPinToInterrupt(PC8), interruptCallback, CHANGE);
+    taskManager.registerEvent(&testing1);
+    taskManager.registerEvent(&testing2);
+    thenTime1 = millis();
+    thenTime2 = millis();
 }
 
 void loop() {
-    // we just print out the contents of the counter every second.
-    Serial.print("Counter is ");
-    Serial.println(counter);
-    delay(1000);
+    taskManager.runLoop();
+
+    if((millis() -thenTime1) > 100) {
+        thenTime1 = millis();
+        testing1.markTriggeredAndNotify();
+    }
+
+//    if((millis() -thenTime2) > 500) {
+//        thenTime2 = millis();
+//        testing2.markTriggeredAndNotify();
+//    }
 }
