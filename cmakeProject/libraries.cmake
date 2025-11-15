08c25804e9462c413cd00d4d6531eded296eb252
dave <dave@thecoderscorner.com>
2025-11-15 21:58:45 +0000

    Allow for both local libraries and auto dependency libs

--- cmakeProject/libraries.cmake
+++ cmakeProject/libraries.cmake
@@ -1,85 +1,104 @@
 cmake_minimum_required(VERSION 3.13)
 
+# Add option to choose library source, ON means you pull libraries into lib/mbed_libs, OFF - auto fetch
+option(USE_LOCAL_LIBRARIES "Use local libraries from lib/ directory instead of FetchContent" ON)
+
 function (includeLibraries)
-include(FetchContent)
+    if(USE_LOCAL_LIBRARIES)
+        message(STATUS "Using local library sources from lib/ directory")
 
-#  Include these libraries in CMakeLists.txt
-# tcMenu
-# IoAbstraction
-# TaskManagerIO
-# tcUnicodeHelper
-# TcMenuLog
-# AdafruitGFXNativePort
-# SimpleCollections
-# LiquidCrystalIO
+        # Use local libraries
+        add_subdirectory(${CMAKE_SOURCE_DIR}/lib/TaskManagerIO/cmake TaskManagerIO)
+        add_subdirectory(${CMAKE_SOURCE_DIR}/lib/IoAbstraction/cmake IoAbstraction)
+        add_subdirectory(${CMAKE_SOURCE_DIR}/lib/SimpleCollections/cmake SimpleCollections)
+        add_subdirectory(${CMAKE_SOURCE_DIR}/lib/tcMenu/cmake tcMenuLib)
+        add_subdirectory(${CMAKE_SOURCE_DIR}/lib/TcMenuLog/cmake TcMenuLog)
+        add_subdirectory(${CMAKE_SOURCE_DIR}/lib/tcUnicodeHelper/cmake TcUnicodeHelper)
+        add_subdirectory(${CMAKE_SOURCE_DIR}/lib/LiquidCrystalIO/cmake LiquidCrystalIO)
+
+        add_subdirectory(${CMAKE_SOURCE_DIR}/mbed_libs/Adafruit-GFX-mbed-fork/cmake AdafruitGFX)
+    else()
+        message(STATUS "Auto fetch TcLibs into _deps folder, to use local libs set: -DUSE_LOCAL_LIBRARIES=ON ")
+
+        include(FetchContent)
+
+        #  Include these libraries in CMakeLists.txt
+        # tcMenu
+        # IoAbstraction
+        # TaskManagerIO
+        # tcUnicodeHelper
+        # TcMenuLog
+        # AdafruitGFXNativePort
+        # SimpleCollections
+        # LiquidCrystalIO
 
 
-SET(TCMENU_BASE_GIT_REPO "https://github.com/TcMenu")
+        SET(TCMENU_BASE_GIT_REPO "https://github.com/TcMenu")
 
-# Fetch the TcMenu git repo
-FetchContent_Declare(
-    tcMenuLib
-    GIT_REPOSITORY ${TCMENU_BASE_GIT_REPO}/tcMenuLib.git
-    GIT_TAG        main # Use a stable branch, tag, or commit hash
-    SOURCE_SUBDIR  cmake
-)
+        # Fetch the TcMenu git repo
+        FetchContent_Declare(
+            tcMenuLib
+            GIT_REPOSITORY ${TCMENU_BASE_GIT_REPO}/tcMenuLib.git
+            GIT_TAG        main # Use a stable branch, tag, or commit hash
+            SOURCE_SUBDIR  cmake
+        )
 
-# Fetch the IoAbstraction git repo
-FetchContent_Declare(
-    IoAbstraction
-    GIT_REPOSITORY ${TCMENU_BASE_GIT_REPO}/IoAbstraction.git
-    GIT_TAG        main # Use a stable branch, tag, or commit hash
-    SOURCE_SUBDIR  cmake
-)
+        # Fetch the IoAbstraction git repo
+        FetchContent_Declare(
+            IoAbstraction
+            GIT_REPOSITORY ${TCMENU_BASE_GIT_REPO}/IoAbstraction.git
+            GIT_TAG        main # Use a stable branch, tag, or commit hash
+            SOURCE_SUBDIR  cmake
+        )
 
-# Fetch the TaskManagerIO git repo
-FetchContent_Declare(
-    TaskManagerIO
-    GIT_REPOSITORY ${TCMENU_BASE_GIT_REPO}/TaskManagerIO.git
-    GIT_TAG        main # Use a stable branch, tag, or commit hash
-    SOURCE_SUBDIR  cmake
-)
+        # Fetch the TaskManagerIO git repo
+        FetchContent_Declare(
+            TaskManagerIO
+            GIT_REPOSITORY ${TCMENU_BASE_GIT_REPO}/TaskManagerIO.git
+            GIT_TAG        main # Use a stable branch, tag, or commit hash
+            SOURCE_SUBDIR  cmake
+        )
 
-# Fetch the TcUnicodeHelper git repo
-FetchContent_Declare(
-    TcUnicodeHelper
-    GIT_REPOSITORY ${TCMENU_BASE_GIT_REPO}/TcUnicodeHelper.git
-    GIT_TAG        main # Use a stable branch, tag, or commit hash
-    SOURCE_SUBDIR  cmake
-)
+        # Fetch the TcUnicodeHelper git repo
+        FetchContent_Declare(
+            TcUnicodeHelper
+            GIT_REPOSITORY ${TCMENU_BASE_GIT_REPO}/TcUnicodeHelper.git
+            GIT_TAG        main # Use a stable branch, tag, or commit hash
+            SOURCE_SUBDIR  cmake
+        )
 
-# Fetch the TcMenuLog git repo
-FetchContent_Declare(
-    TcMenuLog
-    GIT_REPOSITORY ${TCMENU_BASE_GIT_REPO}/TcMenuLog.git
-    GIT_TAG        main # Use a stable branch, tag, or commit hash
-    SOURCE_SUBDIR  cmake
-)
+        # Fetch the TcMenuLog git repo
+        FetchContent_Declare(
+            TcMenuLog
+            GIT_REPOSITORY ${TCMENU_BASE_GIT_REPO}/TcMenuLog.git
+            GIT_TAG        main # Use a stable branch, tag, or commit hash
+            SOURCE_SUBDIR  cmake
+        )
 
-# Fetch the Adafruit-GFX-mbed-fork git repo
-FetchContent_Declare(
-    AdafruitGFX
-    GIT_REPOSITORY ${TCMENU_BASE_GIT_REPO}/Adafruit-GFX-mbed-fork.git
-    GIT_TAG        main # Use a stable branch, tag, or commit hash
-    SOURCE_SUBDIR  cmake
-)
+        # Fetch the Adafruit-GFX-mbed-fork git repo
+        FetchContent_Declare(
+            AdafruitGFX
+            GIT_REPOSITORY ${TCMENU_BASE_GIT_REPO}/Adafruit-GFX-mbed-fork.git
+            GIT_TAG        main # Use a stable branch, tag, or commit hash
+            SOURCE_SUBDIR  cmake
+        )
 
-# Fetch the LiquidCrystalIO git repo
-FetchContent_Declare(
-    LiquidCrystalIO
-    GIT_REPOSITORY ${TCMENU_BASE_GIT_REPO}/LiquidCrystalIO.git
-    GIT_TAG        main # Use a stable branch, tag, or commit hash
-    SOURCE_SUBDIR  cmake
-)
+        # Fetch the LiquidCrystalIO git repo
+        FetchContent_Declare(
+            LiquidCrystalIO
+            GIT_REPOSITORY ${TCMENU_BASE_GIT_REPO}/LiquidCrystalIO.git
+            GIT_TAG        main # Use a stable branch, tag, or commit hash
+            SOURCE_SUBDIR  cmake
+        )
 
-# Fetch the SimpleCollections git repo
-FetchContent_Declare(
-    SimpleCollections
-    GIT_REPOSITORY ${TCMENU_BASE_GIT_REPO}/SimpleCollections.git
-    GIT_TAG        main # Use a stable branch, tag, or commit hash
-    SOURCE_SUBDIR  cmake
-)
+        # Fetch the SimpleCollections git repo
+        FetchContent_Declare(
+            SimpleCollections
+            GIT_REPOSITORY ${TCMENU_BASE_GIT_REPO}/SimpleCollections.git
+            GIT_TAG        main # Use a stable branch, tag, or commit hash
+            SOURCE_SUBDIR  cmake
+        )
 
-FetchContent_MakeAvailable(tcMenuLib IoAbstraction TaskManagerIO TcUnicodeHelper TcMenuLog AdafruitGFX SimpleCollections LiquidCrystalIO)
-
+        FetchContent_MakeAvailable(tcMenuLib IoAbstraction TaskManagerIO TcUnicodeHelper TcMenuLog AdafruitGFX SimpleCollections LiquidCrystalIO)
+    endif ()
 endfunction()
--- tftUserSetups/WroverILI9341.h
+++ tftUserSetups/WroverILI9341.h
@@ -157,7 +157,7 @@
 #define TFT_CS   22  // Chip select control pin D8
 #define TFT_DC   21  // Data Command control pin
 //#define TFT_RST  PIN_D4  // Reset pin (could connect to NodeMCU RST, see next line)
-#define TFT_RST  -1    // Set TFT_RST to -1 if the display RESET is connected to NodeMCU RST or 3.3V
+//#define TFT_RST  -1    // Set TFT_RST to -1 if the display RESET is connected to NodeMCU RST or 3.3V
 
 //#define TFT_BL PIN_D1  // LED back-light (only for ST7789 with backlight control pin)
 
@@ -189,9 +189,9 @@
 // For ESP32 Dev board (only tested with ILI9341 display)
 // The hardware SPI can be mapped to any pins
 
-//#define TFT_MISO 19
-//#define TFT_MOSI 23
-//#define TFT_SCLK 18
+#define TFT_MISO 19
+#define TFT_MOSI 23
+#define TFT_SCLK 18
 //#define TFT_CS   15  // Chip select control pin
 //#define TFT_DC    2  // Data Command control pin
 //#define TFT_RST   4  // Reset pin (could connect to RST pin)
