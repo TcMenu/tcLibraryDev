27c20b0fbbae1a308d0cf9e1371d2ca1d6d4bae6
dave <dave@nutricherry.com>
2026-09-09 08:47:15 +0100

    Remove emulation and run tests natively

---
+++ experimental/unity_config.h
@@ -0,0 +1,24 @@
+#ifndef UNITY_CONFIG_H
+#define UNITY_CONFIG_H
+
+// Empty configuration - rely on Unity defaults
+
+#include <unity.h>
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+void setUp(void) {
+    // set stuff up here
+}
+
+void tearDown(void) {
+    // clean stuff up here
+}
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif // UNITY_CONFIG_H
